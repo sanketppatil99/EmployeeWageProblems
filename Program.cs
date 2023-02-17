620c71c96fb01592d21edc64d43eb901dcb8e0a4
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-17 22:31:03 +0530

    Calling method

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            EmployeeCheck employeeCheck = new EmployeeCheck();
+            employeeCheck.EmployeeAttendance();
         }
     }
 }
