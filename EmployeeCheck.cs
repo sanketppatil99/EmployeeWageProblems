210edc93a8ecfb9d79d9d36cec719f8fd3e455d6
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-17 22:29:29 +0530

    Check employee present or absent

---
+++ EmployeeCheck.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeCheck
+    {
+        public void EmployeeAttendance()
+        {
+            int Is_Full_time = 1;
+
+            Random rand = new Random();
+            int empCheck = rand.Next(2);
+            if (empCheck == Is_Full_time)
+            {
+                Console.WriteLine("Employee is present");
+
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is absent");
+
+            }
+
+
+        }
+    }
+}
