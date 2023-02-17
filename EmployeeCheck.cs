dff3980682eef8ce5720e9eae8657ec505498e50
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-17 22:34:43 +0530

    Calculate Daily wage

--- EmployeeCheck.cs
+++ EmployeeCheck.cs
@@ -12,19 +12,25 @@ namespace EmployeeWage
         {
             int Is_Full_time = 1;
 
+            int Emp_Rate_per_hour = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random rand = new Random();
-            int empCheck = rand.Next(2);
+            int empCheck = rand.Next(3);
+            //Computation
             if (empCheck == Is_Full_time)
             {
-                Console.WriteLine("Employee is present");
-
+                Console.WriteLine("Employee is Full Time");
+                empHrs = 8;
             }
-
             else
             {
-                Console.WriteLine("Employee is absent");
-
+                Console.WriteLine("Employee is not Worked");
+                empHrs = 0;
             }
+            empWage = Emp_Rate_per_hour * empHrs;
+            Console.WriteLine("Employee Wage =  " + empWage);
 
 
         }
