0fabd2fc91a80dfee8fe8411c8f97af2d5f680a4
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 02:26:21 +0530

    Ability to manage total wage interface

--- WageComputation.cs
+++ WageComputation.cs
@@ -6,47 +6,30 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    public class WageComputation
+    public interface IComputeEmpWage
     {
-        public const int IS_PART_TIME = 1, IS_FULL_TIME = 2;
-        string company;
-        int empRatePerHour, numOfWorkingDays, maxHoursPerMonth, totalEmpWage = 0;
-        public WageComputation(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        public void AddCompanyCompanyEmpWage(string company, int maxWorkingDays, int maxWorkingHrs, int empPerHrs);
+        public void IterateOverCompanies();
+    }
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int maxWorkingDays, maxWorkingHrs, empPerHrs;
+        public int totalEmpWage;
+        public CompanyEmpWage(string company, int maxWorkingDays, int maxWorkingHrs, int empPerHrs)
         {
             this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonth = maxHoursPerMonth;
+            this.maxWorkingDays = maxWorkingDays;
+            this.maxWorkingHrs = maxWorkingHrs;
+            this.empPerHrs = empPerHrs;
         }
-        public void ComputeEmployeeWage()
+        public void SetTotalEmpWage(int totalEmpWage)
         {
-            int empHour = 0, workingDays = 0, totalHours = 0;
-            int check;
-            Random random = new Random();
-            while (workingDays <= numOfWorkingDays && totalHours <= maxHoursPerMonth)
-            {
-                workingDays++;
-                check = random.Next(0, 3);
-                switch (check)
-                {
-                    case IS_PART_TIME:
-                        empHour = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHour = 8;
-                        break;
-                    default:
-                        empHour = 0;
-                        break;
-                }
-                totalHours += empHour;
-                Console.WriteLine("Day {0} Employee hours {1}", workingDays, totalHours);
-            }
-            totalEmpWage = totalHours * this.empRatePerHour;
+            this.totalEmpWage = totalEmpWage;
         }
-        public string displaySalary()
+        public override string ToString()
         {
-            return "Total Employee wage for the Company " + company + " is : " + totalEmpWage;
+            return $"Company: {company} EmpPerRateHrs: {empPerHrs} TotalWage: {totalEmpWage}";
         }
     }
 }
