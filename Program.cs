4600d1a30fc202000f3c10b7a3b8ca32b9025ae3
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 02:34:35 +0530

    Refactor to use ArrayList instead of array

--- Program.cs
+++ Program.cs
@@ -4,39 +4,38 @@
     {
         public class EmpWageBuilder : IComputeEmpWage
         {
-            //constants
+            
             public const int IS_FULL_TIME = 1;
             public const int IS_PART_TIME = 2;
-            public int numOfCompany = 0;
-            public CompanyEmpWage[] companies;
+            List<CompanyEmpWage> list;
             public EmpWageBuilder()
             {
-                companies = new CompanyEmpWage[3];
+                
+                list = new List<CompanyEmpWage>();
             }
             public void AddCompanyCompanyEmpWage(string company, int maxWorkingDays, int maxWorkingHrs, int empPerHrs)
             {
                 CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, maxWorkingDays, maxWorkingHrs, empPerHrs);
-                companies[numOfCompany] = companyEmpWage;
-                numOfCompany++;
+                list.Add(companyEmpWage);
             }
             public void IterateOverCompanies()
             {
-                for (int i = 0; i < companies.Length; i++)
+                for (int i = 0; i < list.Count; i++)
                 {
-                    int totalWage = ComputeEmpWage(companies[i]);
-                    companies[i].SetTotalEmpWage(totalWage);
-                    Console.WriteLine(companies[i]);
+                    int totalWage = ComputeEmpWage(list[i]);
+                    list[i].SetTotalEmpWage(totalWage);
+                    Console.WriteLine(list[i]);
                 }
             }
             public int ComputeEmpWage(CompanyEmpWage details)
             {
-                //local variables
+                
                 int empHrs = 0, empWage = 0, day = 1, totalEmpWage = 0, totalEmpHrs = 0;
 
                 Random random = new Random();
                 while (day <= details.maxWorkingDays && totalEmpHrs <= details.maxWorkingHrs)
                 {
-                    int empAttendence = random.Next(0, 3);//0,1,2
+                    int empAttendence = random.Next(0, 3);
                     switch (empAttendence)
                     {
                         case IS_FULL_TIME:
@@ -56,18 +55,15 @@
                 }
                 return totalEmpWage;
             }
-            public static void Main(string[] args)
+            static void Main(string[] args)
             {
-                Console.WriteLine("Welcome to Employee Wage Computation");
-                
+                Console.WriteLine("Welcome to the Employee Wage Computation\n--------------------------------------------\n");
                 EmpWageBuilder builder = new EmpWageBuilder();
                 builder.AddCompanyCompanyEmpWage("TCS", 25, 10, 20);
                 builder.AddCompanyCompanyEmpWage("Capgemini", 25, 20, 30);
                 builder.AddCompanyCompanyEmpWage("Wipro", 35, 30, 45);
                 builder.IterateOverCompanies();
                 Console.ReadLine();
-
-
             }
         }
     }
