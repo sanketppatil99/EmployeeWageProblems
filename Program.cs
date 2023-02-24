21c799c2cab1f4b1baf9ceae629701b062f61c2e
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2023-02-25 00:57:34 +0530

    Calculate Wage for Multiple Companies

--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
             EmployeeCheck employeeCheck = new EmployeeCheck();
-            employeeCheck.EmployeeAttendance();
+            employeeCheck.ComputeEmpWage("TCS", 25, 10, 20);
+            employeeCheck.ComputeEmpWage("Capgemini", 25, 20, 30);
+            employeeCheck.ComputeEmpWage("Wipro", 35, 30, 45);
         }
     }
 }
